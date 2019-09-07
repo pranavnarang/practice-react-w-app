a71596d1f12b950b81dd0f28d8d1620087a79bc0
Pranav <pnarang1130@gmail.com>
2019-09-07 10:18:40 -0700

    added styles and finished v1 of project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Weather from "./components/weather"
 
 const API_KEY = "3ae01fa0fbc8643e12a00c56cf570895";
 
-class App extends React.Component{
-    
-    state =  {
+class App extends React.Component {
+
+    state = {
         temperature: undefined,
         city: undefined,
         country: undefined,
@@ -24,15 +24,15 @@ class App extends React.Component{
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
         console.log(data)
-        
+
         if (city && country) {
             this.setState({
                 temperature: data.main.temp,
                 city: data.name,
                 country: data.sys.country,
                 humidity: data.main.humidity,
-                description: data.weather[0].description,  
-            }) 
+                description: data.weather[0].description,
+            })
         } else {
             this.setState({
                 error: "Please enter the values"
@@ -40,19 +40,25 @@ class App extends React.Component{
         }
     }
 
-    render(){
-        return(
-            <div>
-                <Titles />
-                <Form getWeather={this.getWeather}/>
-                <Weather 
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                 />
+    render() {
+        return (
+            <div className="background">
+                <div className="weather__container">
+                    <div className="weather__container-left">
+                        <Titles />
+                    </div>
+                    <div className="weather__container-right">
+                        <Form getWeather={this.getWeather} />
+                        <Weather
+                            temperature={this.state.temperature}
+                            city={this.state.city}
+                            country={this.state.country}
+                            humidity={this.state.humidity}
+                            description={this.state.description}
+                            error={this.state.error}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,11 +1,11 @@
 import React from "react"
 
 const Weather = props => (
-    <div>
-        {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidity && <p>Humidity: {props.humidity}</p>}
-        {props.description && <p>Description: {props.description}</p>}
+    <div class="weather__information">
+        {props.city && props.country && <p> <span class="weather__key">Location:</span> {props.city}, {props.country}</p>}
+        {props.temperature && <p><span class="weather__key">Temperature:</span> {props.temperature}</p>}
+        {props.humidity && <p><span class="weather__key">Humidity:</span> {props.humidity}</p>}
+        {props.description && <p><span class="weather__key">Description:</span> {props.description}</p>}
         {props.error && <p>{props.error}</p>}
     </div>
 )
